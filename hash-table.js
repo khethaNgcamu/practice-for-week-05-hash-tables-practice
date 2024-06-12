f13d008f1fe1eddcd8ae1906e25d23937fc957b2
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-06-12 16:36:58 +0200

    hash table practice part1 completed

--- hash-table.js
+++ hash-table.js
@@ -12,14 +12,30 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
+    this.count = 0;
   }
 
   hash(key) {
     // Your code here
+
+     // Step 1: Hash the key using SHA-256
+     const hashValue = sha256(key);
+
+     // Step 2: Extract the first 8 characters
+     const first8Chars = hashValue.substring(0, 8);
+ 
+     // Step 3: Convert these 8 characters from hexadecimal to integer
+     const intHashValue = parseInt(first8Chars, 16);
+ 
+     return intHashValue;
   }
 
   hashMod(key) {
     // Your code here
+    const hashValue = this.hash(key);
+    return hashValue % this.capacity;
   }
 
   insertNoCollisions(key, value) {
